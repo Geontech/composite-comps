0bda7a853f72c7f4bcc3fcb4307c9f27b3dc3541
Justin Albright <jalbright@geontech.com>
2025-05-05 14:51:40 -0400

    fix typo

--- src/components/udp_source/socket/dpdk.cpp
+++ src/components/udp_source/socket/dpdk.cpp
@@ -365,7 +365,6 @@ auto dpdk_udp::receive(std::stop_token token) -> void {
 
             ++attempt;
         } while (attempt < max_attempts);
-        }
     }
     m_logger->trace("Queue configured");
 
